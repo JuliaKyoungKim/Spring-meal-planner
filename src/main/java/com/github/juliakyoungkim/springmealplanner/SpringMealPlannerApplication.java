76033b69c968569b000dead87b0b97eca8d2ad0d
unknown <joulia.kim@gmail.com>
2020-02-06 12:25:07 +0900

    first commit

---
+++ src/main/java/com/github/juliakyoungkim/springmealplanner/SpringMealPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.juliakyoungkim.springmealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMealPlannerApplication.class, args);
+	}
+
+}
